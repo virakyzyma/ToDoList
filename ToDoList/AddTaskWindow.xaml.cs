0d6ca49ee1a7e394845774d770362adea2d25062
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-07-22 15:56:39 +0300

    Add project files.

---
+++ ToDoList/AddTaskWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for AddTaskWindow.xaml
+    /// </summary>
+    public partial class AddTaskWindow : Window
+    {
+        private ObservableCollection<Task> tasks;
+        public AddTaskWindow(ObservableCollection<Task> tasksList)
+        {
+            InitializeComponent();
+            tasks = tasksList;
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string name = taskNameTextBox.Text;
+            string status = ((ComboBoxItem)taskStatusComboBox.SelectedItem).Content.ToString();
+            tasks.Add(new Task(name, status));
+        }
+    }
+}
---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Task> tasks;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<Task>();
+            taskListView.ItemsSource = tasks;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            // Откройте окно для добавления задачи и передайте tasks для обновления после добавления задачи
+            AddTaskWindow addTaskWindow = new AddTaskWindow(tasks);
+            addTaskWindow.ShowDialog();
+        }
+
+        private void ChangeStatus_Click(object sender, RoutedEventArgs e)
+        {
+            // Изменение статуса задачи
+            Button button = (Button)sender;
+            Task task = (Task)button.Tag;
+            task.Status = (task.Status == "выполнено") ? "не выполнено" : "выполнено";
+        }
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            // Удаление задачи
+            Button button = (Button)sender;
+            Task task = (Task)button.Tag;
+            tasks.Remove(task);
+        }
+
+        private void taskListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    public class Task : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        private string status;
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        public Task(string name, string status)
+        {
+            Name = name;
+            Status = status;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
